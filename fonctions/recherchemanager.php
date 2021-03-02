3fb35ee21a540ab8de91dde2d42b2ca6a5b1b417
Vixus07 <millot007@gmail.com>
2021-03-02 15:03:57 +0100

    Ajoute des affiches de livres

--- fonctions/recherchemanager.php
+++ fonctions/recherchemanager.php
@@ -10,7 +10,7 @@ class RechercheManager
   //Rechercher une list d'item selon leurs catégorie
   public function news(Recherche $obj)
   {
-    $pre = $this->db->prepare('SELECT titre,affiche FROM item WHERE categorie = :categorie ORDER BY dateitem ASC');
+    $pre = $this->db->prepare('SELECT titre,affiche FROM item WHERE categorie = :categorie ORDER BY dateitem DESC');
 
     $pre->execute(array(
       'categorie' => $obj->getCategorie()
@@ -21,10 +21,14 @@ class RechercheManager
     $i = 0;
     foreach ($resultat as $row) {
       echo "<figure>
-              <img class=\"newsaffiche\" src=\"".$row["affiche"]."\" alt=\"Affiche du livre: ".$row["titre"]."\" width=\"150px\">
+              <img src=\"".$row["affiche"]."\" alt=\"Affiche du livre: ".$row["titre"]."\" width=\"150px\">
               <figcaption>".$row["titre"]."</figcaption>
             </figure>";
       $i++;
+      if($i>10)
+      {
+        break;
+      }
     }
   }
 }
