e3fed01bff7b9fd943c8684dab7c5dc10a980b27
CBarneron <71774149+CBarneron@users.noreply.github.com>
2021-03-04 09:55:57 +0100

    Avancement sur publi

---
+++ fonctions/publi.php
@@ -0,0 +1,40 @@
+<?php
+class Publication
+{
+  private $Publi;
+  private $Idpubli;
+  private $Idadmin;
+  private $Iditem;
+
+
+  private $titre;
+  private $affiche;
+//constructeur
+  public function __construct($Publi, $Idpubli, $Idadmin,$Iditem)
+  {
+
+    $this->publi = $Publi
+    $this->idpubli = $Idpubli
+    $this->idadmin = $Idadmin
+    $$this->iditem = $Iditem
+
+  }
+//Setteur
+  public function setSearch($search)
+  {
+
+  }
+
+//Getteur
+  public function getSearch()
+  {
+
+  }
+
+//Methodes
+  public function Actus($titre, $affiche)
+  {
+    //
+  }
+}
+?>
